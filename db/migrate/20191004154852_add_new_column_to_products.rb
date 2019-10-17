2c33c0f3d8246834c8504774a364aae54a946c8b
Sean Altemose <sean@seanaltemose.com>
2019-10-17 16:09:50 +0000

    Automatically backed up by Learn

---
+++ db/migrate/20191004154852_add_new_column_to_products.rb
@@ -0,0 +1,5 @@
+class AddNewColumnToProducts < ActiveRecord::Migration[5.0]
+  def change
+    add_column :products, :description, :text
+  end
+end
---
+++ db/migrate/20191004155132_add_field_to_products.rb
@@ -0,0 +1,5 @@
+class AddFieldToProducts < ActiveRecord::Migration[5.0]
+  def change
+    add_column :products, :inventory, :integer
+  end
+end
